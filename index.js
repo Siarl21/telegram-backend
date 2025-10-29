2e332eca35b22f8faf3979d5cb8f24ef60a8ea25
Siarl21 <carlos.bob.21@hotmail.com>
2025-10-29 01:23:17 -0600

    Fix: usar puerto Railway

--- index.js
+++ index.js
@@ -75,5 +75,7 @@ app.post("/send-telegram", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ API escuchando en :${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`✅ API escuchando en :${PORT}`)
+);
